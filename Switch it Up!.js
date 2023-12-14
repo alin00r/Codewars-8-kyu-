956bb6e4c07b606b433945237162b95c43e2cce1
alin00r <alinoorspam@gmail.com>
2023-12-14 20:05:29 +0200

    Switch it Up!

---
+++ Switch it Up!.js
@@ -0,0 +1,17 @@
+function switchItUp(number) {
+  let words = [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ];
+  return words[number];
+}
+
+console.log(switchItUp(0));
